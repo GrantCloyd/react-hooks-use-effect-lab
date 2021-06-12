2486a2d599167e88906031c9fa738a44b7f9e022
Grant Cloyd <dgcloyd@gmail.com>
2021-06-12 15:54:27 -0500

    Done.

--- src/components/App.js
+++ src/components/App.js
@@ -1,41 +1,39 @@
-import React, { useState } from "react";
-import Question from "./Question";
-import quiz from "../data/quiz";
+import React, { useState, useCallback } from "react"
+import Question from "./Question"
+import quiz from "../data/quiz"
 
 function App() {
-  const [questions, setQuestions] = useState(quiz);
-  const [currentQuestionId, setCurrentQuestion] = useState(1);
-  const [score, setScore] = useState(0);
-  const currentQuestion = questions.find((q) => q.id === currentQuestionId);
+   const [questions, setQuestions] = useState(quiz)
+   const [currentQuestionId, setCurrentQuestion] = useState(1)
+   const [score, setScore] = useState(0)
 
-  function handleQuestionAnswered(correct) {
-    if (currentQuestionId < questions.length) {
-      setCurrentQuestion((currentQuestionId) => currentQuestionId + 1);
-    } else {
-      setCurrentQuestion(null);
-    }
-    if (correct) {
-      setScore((score) => score + 1);
-    }
-  }
+   const currentQuestion = questions.find(q => q.id === currentQuestionId)
 
-  return (
-    <main>
-      <section>
-        {currentQuestion ? (
-          <Question
-            question={currentQuestion}
-            onAnswered={handleQuestionAnswered}
-          />
-        ) : (
-          <>
-            <h1>Game Over</h1>
-            <h2>Total Correct: {score}</h2>
-          </>
-        )}
-      </section>
-    </main>
-  );
+   function handleQuestionAnswered(correct) {
+      if (currentQuestionId < questions.length) {
+         setCurrentQuestion(currentQuestionId => currentQuestionId + 1)
+      } else {
+         setCurrentQuestion(null)
+      }
+      if (correct) {
+         setScore(score => score + 1)
+      }
+   }
+
+   return (
+      <main>
+         <section>
+            {currentQuestion ? (
+               <Question question={currentQuestion} onAnswered={handleQuestionAnswered} />
+            ) : (
+               <>
+                  <h1>Game Over</h1>
+                  <h2>Total Correct: {score}</h2>
+               </>
+            )}
+         </section>
+      </main>
+   )
 }
 
-export default App;
+export default App
